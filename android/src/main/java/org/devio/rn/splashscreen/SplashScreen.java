41e7eca7d84f76ca18fb49ebe82d36465e843708
sayednbays <zoray2ksayed@gmail.com>
2020-08-14 00:13:29 +0800

    ViewCompat.requestApplyInsets(decorView);

--- android/src/main/java/org/devio/rn/splashscreen/SplashScreen.java
+++ android/src/main/java/org/devio/rn/splashscreen/SplashScreen.java
@@ -114,6 +114,7 @@ public class SplashScreen {
                                 defaultInsets.getSystemWindowInsetBottom());
                     }
                 });
+                ViewCompat.requestApplyInsets(decorView);
             }
         }
 
